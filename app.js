a938e90a60a7daeb4a848224fafdc888345dbe6f
Benjamin Ek <benjaminek@live.se>
2020-11-09 14:48:19 +0100

    routes/courses.js can get local json

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const path = require("path");
+const coursesRouter = require("./routes/courses")
+const fs = require("fs");
     
 // Skapa en instans av express
 const app = express();
     
 // Skapa statisk sökväg
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use("/courses", coursesRouter);
     
 // Port för anslutning
 const port = process.env.PORT || 3000;
 
 app.get("/api/test", (req, res) => {
-    res.send({"message":"lyckat"});
+    let rawCourses = fs.readFileSync("./my-courses.json");
+    let courses = JSON.parse(rawCourses);
+    res.send(courses);
 });
     
 // Starta servern
--- routes/courses.js
+++ routes/courses.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const fs = require("fs");
+const path = require("path");
 
-router.get("/courses", (req, res) => {
-    res.send({ "message":"gjort"});
+
+router.get("/", (req, res) => {
+    let rawCourses = fs.readFileSync(path.resolve(__dirname + "/../my-courses.json"));
+    let courses = JSON.parse(rawCourses);
+    res.send(courses);
 });
 
 module.exports = router;
