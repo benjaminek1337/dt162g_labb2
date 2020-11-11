19574017e98e539c2d0b0c666247532a4645e83f
Benjamin Ek <benjaminek@live.se>
2020-11-11 12:07:32 +0100

    chagned inports/added functions for getting by id

--- routes/api.js
+++ routes/api.js
@@ -2,24 +2,35 @@ const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 const path = require("path");
+const courses = require("../courses.json");
+const myCourses = require("../my-courses.json");
+const subjects = require("../subjects.json");
 
-const rawCourses = fs.readFileSync(path.resolve(__dirname + "/../courses.json"));
-let courses = JSON.parse(rawCourses).courses;
-const rawSubjects = fs.readFileSync(path.resolve(__dirname + "/../subjects.json"));
-let subjects = JSON.parse(rawSubjects).subjects;
-const rawMyCourses = fs.readFileSync(path.resolve(__dirname + "/../my-courses.json"));
-let myCourses = JSON.parse(rawMyCourses).myCourses;
+// const rawCourses = fs.readFileSync(path.resolve(__dirname + "/../courses.json"));
+// let courses = JSON.parse(rawCourses).courses;
+// const rawSubjects = fs.readFileSync(path.resolve(__dirname + "/../subjects.json"));
+// let subjects = JSON.parse(rawSubjects).subjects;
+// const rawMyCourses = fs.readFileSync(path.resolve(__dirname + "/../my-courses.json"));
+// let myCourses = JSON.parse(rawMyCourses).myCourses;
 
-function getSubject(name){
-    return subjects.find(s => s.subjectCode == name)
+function getSubjectBySubjectCode(code){
+    return subjects.subjects.find(s => s.subjectCode == code);
+}
+
+function getCourseByCourseCode(code){
+    return courses.courses.find(c => c.courseCode == code);
+}
+
+function getMyCourseByCourseCode(code){
+    return myCourses.myCourses.find(c => c.courseCode == code);
 }
 
 router.get("/courses", (req, res) => {
-    let output = courses;
+    let output = courses.courses;
 
     for (let i = 0; i < output.length; i++) {
         const course = output[i];
-        let sub = getSubject(course.subjectCode);
+        let sub = getSubjectBySubjectCode(course.subjectCode);
         if(sub != undefined)
             course.subject = sub.subject;
     }
@@ -28,21 +39,26 @@ router.get("/courses", (req, res) => {
 
 router.get("/courses/:id", (req, res) => {
     const name = req.params.id;
-    course = courses.find(c => c.courseCode == name.toLocaleUpperCase());
-    course.subject = getSubject(course.subjectCode).subject;
-    res.send(course);
+    course = getCourseByCourseCode(name.toLocaleUpperCase());
+    if(course != undefined){
+        course.subject = getSubjectBySubjectCode(course.subjectCode).subject;
+        res.send(course);
+    } else {
+        res.sendStatus(404);
+    }
 });
 
 router.get("/my/courses", (req, res) => {
     let output = [];
-    for (let i = 0; i < myCourses.length; i++) {
-        const mc = myCourses[i];
-        let filteredcourses = courses.find(co => co.courseCode == mc.courseCode);
-        let sub = getSubject(filteredcourses.subjectCode);
-        filteredcourses.completed = mc.completed;
-        if(sub != undefined)
-            filteredcourses.subject = sub.subject;
-        output.push(filteredcourses);
+    for (let i = 0; i < myCourses.myCourses.length; i++) {
+        const mc = myCourses.myCourses[i];
+        console.log(mc);
+        let course = getCourseByCourseCode(mc.courseCode);
+        let subject = getSubjectBySubjectCode(course.subjectCode);
+        course.completed = mc.completed;
+        if(subject != undefined)
+            course.subject = subject.subject;
+        output.push(course);
     }
     res.send(output);
 });
@@ -50,9 +66,9 @@ router.get("/my/courses", (req, res) => {
 router.get("/my/courses/:id", (req, res) => {
     const name = req.params.id;
     
-    let course = courses.find(c => c.courseCode == name.toLocaleUpperCase());
-    let sub = getSubject(course.subjectCode);
-    let myCourse = myCourses.find(c => c.courseCode == course.courseCode);
+    let course = getCourseByCourseCode(name.toLocaleUpperCase());
+    let sub = getSubjectBySubjectCode(course.subjectCode);
+    let myCourse = getMyCourseByCourseCode(course.courseCode);
     try{
         if(sub != undefined)
             course.subject = sub.subject;
@@ -60,17 +76,17 @@ router.get("/my/courses/:id", (req, res) => {
         res.send(course);
     }
     catch(error){
-        res.send(error)
+        res.sendStatus(404)
     }
 });
 
 router.get("/subjects", (req, res) => {
-    res.send(subjects);
+    res.send(subjects.subjects);
 });
 
 router.get("/subjects/:id", (req, res) => {
     const name = req.params.id;
-    res.send(subjects.find(s => s.subjectCode == name.toLocaleUpperCase()));
+    res.send(getSubjectBySubjectCode(name.toLocaleUpperCase()));
 });
 
 router.post("/my/courses/add/", (req, res) => {
@@ -79,16 +95,16 @@ router.post("/my/courses/add/", (req, res) => {
         courseCode: req.body.courseCode,
         completed: req.body.done
     }
-
+    myCourses.myCourses.push(newCourse);
     res.send(newCourse);
 });
 
 router.put("/my/courses/:id", (req, res) => {
     const name = req.params.id;
-    const mc = myCourses.find(c => c.courseCode == name.toLocaleUpperCase());
+    const mc = getMyCourseByCourseCode(name.toLocaleUpperCase());
     if(mc != undefined){
         mc.completed = req.body.done;
-        res.sendStatus(204);
+        res.send(mc);
     } else {
         res.sendStatus(404);
     }
@@ -96,11 +112,11 @@ router.put("/my/courses/:id", (req, res) => {
 
 router.delete("/my/courses/:id", (req, res) => {
     const name = req.params.id;
-    const mc = myCourses.find(c => c.courseCode == name.toLocaleUpperCase());
+    const mc = getMyCourseByCourseCode(name.toLocaleUpperCase());
     if(mc != undefined){
-        let index = myCourses.indexOf(mc)
+        let index = myCourses.myCourses.indexOf(mc)
         delete mc;
-        myCourses.splice(index, 1);
+        myCourses.myCourses.splice(index, 1);
         res.sendStatus(204);
     } else {
         res.sendStatus(404);
